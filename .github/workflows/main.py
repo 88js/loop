ad9ab1c5baf95f197f61c6381dfca9fd0cd333c2
migho ahmed <141735788+88js@users.noreply.github.com>
2023-08-08 04:58:56 -0700

    Create main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,17 @@
+from requests import get
+from time import sleep
+
+      
+def migho():
+    try:
+        sleep(20)
+        get('https://jhtufdsdxgcfhbjnhbgjfhdxxdgfhgj.saraahmed65.repl.co')
+    except:
+        migho()
+
+try:
+    while True:
+        migho()
+except:
+    while True:
+        migho()
